a41933b56336cc417b5a3e04ad16576f9b63c669
s-egge <ssteele836@gmail.com>
2024-03-19 13:46:32 -0700

    Replaced homepage buttons with a logo
    No need for the buttons now that we have a navbar

--- app/src/main/java/edu/oregonstate/cs492/bookkeeper/ui/HomePageFragment.kt
+++ app/src/main/java/edu/oregonstate/cs492/bookkeeper/ui/HomePageFragment.kt
@@ -1,32 +1,14 @@
 package edu.oregonstate.cs492.bookkeeper.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import edu.oregonstate.cs492.bookkeeper.R
 
 class HomePageFragment : Fragment(R.layout.fragment_home) {
     private val tag: String = "HomePageFragment"
-    private lateinit var libraryBtn: Button
-    private lateinit var browseBtn: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        libraryBtn = view.findViewById(R.id.btn_your_library)
-        browseBtn = view.findViewById(R.id.btn_browse_books)
-
-        libraryBtn.setOnClickListener{
-            val directions = HomePageFragmentDirections.navigateToLibrary()
-            findNavController().navigate(directions)
-        }
-
-        browseBtn.setOnClickListener{
-            val directions = HomePageFragmentDirections.navigateToBrowseBooks()
-            findNavController().navigate(directions)
-        }
     }
 }
