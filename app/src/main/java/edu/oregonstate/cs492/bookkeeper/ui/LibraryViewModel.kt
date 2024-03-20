19fab54c51673be4089cea5b5ed8c1ea5d0c1466
JudeWi <86169929+JudeWi@users.noreply.github.com>
2024-03-20 16:35:39 -0700

    Willijud final merge (#18)
    * LibraryFragment:
    - Books added to DB are displayed with coverURL
    - Percentage of book read displayed top right
    - Dynamic w/ recyclerView.
    - LibraryBook cards are clickable, book name logged to terminal
    
    Note: To test, add books from browsing section and navigate back to library, createTestingData() entries will not work, covers will not display.
    LibraryDao:
    - Added deleteAll() method, deletes all entries in library table
    LibraryRepository:
    - Added deleteAllBooks() method, calls deleteAll() in dao
    LibraryViewModel:
    - Added removeAllBooks() method, calls deleteAllBooks() in repo
    
    * Added search in library, added recent searches.
    
    Search results non-functional.
    
    * search library fix
    
    * Fixed bottomNav issue (janky solution)
    
    ---------
    
    Co-authored-by: s-egge <ssteele836@gmail.com>

--- app/src/main/java/edu/oregonstate/cs492/bookkeeper/ui/LibraryViewModel.kt
+++ app/src/main/java/edu/oregonstate/cs492/bookkeeper/ui/LibraryViewModel.kt
@@ -1,10 +1,12 @@
 package edu.oregonstate.cs492.bookkeeper.ui
 
 import android.app.Application
+import androidx.constraintlayout.helper.widget.Flow
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.viewModelScope
 import edu.oregonstate.cs492.bookkeeper.data.AppDatabase
 import edu.oregonstate.cs492.bookkeeper.data.BookSearch
@@ -18,6 +20,9 @@ class LibraryViewModel(application: Application) : AndroidViewModel(application)
     )
 
     val libraryBooks = repository.getAllBooks().asLiveData()
+
+    private val _searchResults = MutableLiveData<List<LibraryBook>?>(null)
+    val searchResults: LiveData<List<LibraryBook>?> = _searchResults
 
     fun addBook(book: LibraryBook) {
         viewModelScope.launch {
@@ -42,4 +47,11 @@ class LibraryViewModel(application: Application) : AndroidViewModel(application)
 
     fun getBookByTitleOrAuthor(query: String) =
         repository.getBookByTitleOrAuthor(query).asLiveData()
+
+    fun loadSearchResults(query: String) {
+        viewModelScope.launch {
+            val result = repository.getBookByTitleOrAuthor(query)
+            _searchResults.value = result.asLiveData().value
+        }
+    }
 }
